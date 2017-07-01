c2ebe5068312fbf2b1da4837cd44e60b85d99508
熊伟 <597370561@qq.com>
2017-07-01 10:15:48 +0800

    finished blue lib

---
+++ BlueLib/BlueLib/Consts.swift
@@ -0,0 +1,11 @@
+//
+//  Consts.swift
+//  BlueLib
+//
+//  Created by 熊伟 on 2017/7/1.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+import Foundation
+
+let downloadImageNotification        = "BLDownloadImageNotification"

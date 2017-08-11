0fa737c7ccfaa21487d4c87163d8e96896ececc9
xiongwei3 <xiongwei3@tuniu.com>
2017-08-11 16:24:33 +0800

    [xiongwei3][modify]pod init

---
+++ RouteManager/RouteManager/BGRouteService.swift
@@ -0,0 +1,24 @@
+//
+//  BGRouteService.swift
+//  RouteManager
+//
+//  Created by Bear on 2017/8/11.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+import UIKit
+
+class BGRouteService: NSObject {
+
+    func to(route:String) -> Bool {
+        
+        
+        
+        
+        
+        return true
+    }
+    
+    
+    
+}
--- RouteManager/RouteManager/ViewController.swift
+++ RouteManager/RouteManager/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jump: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        jump.addTarget(self, action: #selector(handleJumpButtonClicked), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func handleJumpButtonClicked() {
+        let options = [UIApplicationOpenURLOptionUniversalLinksOnly : false]
+        let url = URL.init(string: "https://www.baidu.com")
+        guard url != nil else {
+            return
+        }
+        UIApplication.shared.open(url!, options: options, completionHandler: nil)
     }
 
 
